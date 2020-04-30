31731fded4dc1fa13b9a927cddf7998ed02988d6
mike <hornegger@view-elevator.com>
2020-04-30 14:05:16 +0200

    handle auth error when sending requests

--- src/_services/todo.service.js
+++ src/_services/todo.service.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import {authService} from "./auth.service";
 import {authHeader} from "../_helpers";
 
 export const todoService = {
@@ -14,11 +15,13 @@ function updateTodo(todo) {
         completed: todo.completed
     }
 
-    return axios.put(`http://localhost:8090/api/v1/todo/${todo.id}`, request, {headers: authHeader()});
+    return axios.put(`http://localhost:8090/api/v1/todo/${todo.id}`, request, {headers: authHeader()})
+        .then(handleResponse);
 }
 
 function deleteTodo(id) {
-    return axios.delete(`http://localhost:8090/api/v1/todo/${id}`, {headers: authHeader()});
+    return axios.delete(`http://localhost:8090/api/v1/todo/${id}`, {headers: authHeader()})
+        .then(handleResponse);
 }
 
 function addTodo(newTodo) {
@@ -27,8 +30,28 @@ function addTodo(newTodo) {
         title,
         completed
     }, {headers: authHeader()})
+        .then(handleResponse);
 }
 
 function getAll() {
     return axios.get('http://localhost:8090/api/v1/todo', {headers: authHeader()})
+        .then(handleResponse);
+}
+
+function handleResponse(response) {
+    return response.text().then(text => {
+        const data = text && JSON.parse(text);
+        if (!response.ok) {
+            if (response.status === 401 || response.status === 403) {
+                // auto logout if 401 response returned from api
+                authService.logout();
+                location.reload();
+            }
+
+            const error = (data && data.message) || response.statusText;
+            return Promise.reject(error);
+        }
+
+        return data;
+    });
 }
